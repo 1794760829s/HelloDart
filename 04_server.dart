647e2d750d098a239a9b6991d064b52ea9f16a84
1794760829s <1250994169@qq.com>
2022-06-28 13:04:24 +0800

    6.28 13:04

---
+++ 04_server.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+void main() async {
+  final requests = await HttpServer.bind('localhost', 8889);
+  await for (final request in requests) {
+    processRequest(request);
+  }
+}
+
+void processRequest(HttpRequest request) {
+  print('Got request for ${request.uri.path}');
+  final response = request.response;
+  if (request.uri.path == '/dart') {
+    response
+      ..headers.contentType = ContentType(
+        'text',
+        'plain',
+      )
+      ..write('Hello from the server');
+  } else {
+    response.statusCode = HttpStatus.notFound;
+  }
+  response.close();
+}
